fde6a5632bd638dc5764b2e434cb82bc7a7e5fb5
TimoxaV <vadim2493@gmail.com>
2020-09-08 15:21:23 +0300

    fixed to meet checkstyle

--- src/main/java/com/internet/shop/dao/OrderDao.java
+++ src/main/java/com/internet/shop/dao/OrderDao.java
@@ -2,7 +2,6 @@ package com.internet.shop.dao;
 
 import com.internet.shop.model.Order;
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderDao extends GenericDao<Order, Long> {
     List<Order> getUserOrders(Long userId);
--- src/main/java/com/internet/shop/dao/ShoppingCartDao.java
+++ src/main/java/com/internet/shop/dao/ShoppingCartDao.java
@@ -1,7 +1,6 @@
 package com.internet.shop.dao;
 
 import com.internet.shop.model.ShoppingCart;
-import java.util.List;
 import java.util.Optional;
 
 public interface ShoppingCartDao extends GenericDao<ShoppingCart, Long> {
