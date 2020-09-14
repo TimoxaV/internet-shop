43ec04d8245ff489fc2a1b898fb6f693db023e5e
TimoxaV <vadim2493@gmail.com>
2020-09-14 22:08:57 +0300

    added login cotroller and auth filter

---
+++ src/main/java/com/internet/shop/security/AuthenticationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.internet.shop.security;
+
+import com.internet.shop.exceptions.AuthenticationException;
+import com.internet.shop.lib.Inject;
+import com.internet.shop.lib.Service;
+import com.internet.shop.model.User;
+import com.internet.shop.service.UserService;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String login, String password) throws AuthenticationException {
+        User user = userService.findByLogin(login).orElseThrow(() ->
+                new AuthenticationException("Incorrect login!"));
+        if (user.getPassword().equals(password)) {
+            return user;
+        }
+        throw new AuthenticationException("Incorrect password");
+    }
+}
